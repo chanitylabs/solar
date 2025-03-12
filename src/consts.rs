68541a4cc166f6e6f7aae390e5787181e178633f
Greg <kotsmilewtm@gmail.com>
2025-03-12 17:03:30 +0300

    add openbook

--- src/consts.rs
+++ src/consts.rs
@@ -2,6 +2,8 @@ pub const RAYDIUM_AMM_PROGRAM: &str = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1
 pub const SOL_USDC_POOL_USDC_VAULT: &str = "HLmqeL62xR1QoZ1HKKbXRrdN1p3phKpxRMb2VVopvBBz";
 pub const SOL_USDC_POOL_SOL_VAULT: &str = "DQyrAcCrDXQ7NeoqGgDCZwBvWDcYmFCjSb9JtteuvPpz";
 
+pub const OPENBOOK_PROGRAM: &str = "srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX";
+
 pub const SOL_ADDRESS: &str = "So11111111111111111111111111111111111111112";
 pub const SOL_DECIMALS: u8 = 9;
 
